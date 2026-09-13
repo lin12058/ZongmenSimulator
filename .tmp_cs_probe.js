ba86c730ff43b0b9d1812c4a242df23f2b5de61f
6g3y <372091066@qq.com>
2026-09-13 13:52:36 +0800

    清理: .tmp_* 临时探针脚本移出版本库并加 gitignore（并行会话工作文件误入库）

--- .tmp_cs_probe.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const cs = fs.readFileSync('Server/Zongmen/Services/MapWorldService.cs', 'utf8');
-const lines = cs.split('\n');
-console.log('=== roadVer / tile freshness lines ===');
-lines.forEach((l, i) => { if (/roadVer|RoadVer|tileRev|TileRev|rev|Rev/.test(l)) console.log((i + 1) + ': ' + l.trim().substring(0, 140)); });
-console.log('\n=== Store/batch/backfill mentions ===');
-lines.forEach((l, i) => { if (/Store\(|Batch|WriteBack|250ms|flush|Flush|blockingtile/i.test(l)) console.log((i + 1) + ': ' + l.trim().substring(0, 140)); });
